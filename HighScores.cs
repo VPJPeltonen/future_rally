becbc064da04dbbb8716dc43aa55ae02639d1b20
VPJP <vpjpeltonen@gmail.com>
2019-07-04 15:59:26 +0300

    some high scores stuff

---
+++ HighScores.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class HighScores : MonoBehaviour
+{
+    /*
+    TT = total times
+    LT = Lap Times
+     */
+    public static List<string> TTracernames = new List<string>();
+    public static List<string> LTracernames = new List<string>();
+    public static List<float> TTtimes = new List<float>();
+    public static List<float> LTtimes = new List<float>();
+
+    public static Dictionary<string,float> TTscores=  new Dictionary<string, float>();
+    public static Dictionary<string,float> LTscores=  new Dictionary<string, float>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayScores(){
+
+    }
+
+    private void orderscores(Dictionary<string,float> list){
+        string tempstr = "";
+        //int posCount = 1;
+        //int Pos = 0;
+        var items = from pair in list
+                    orderby pair.Value ascending
+                    select pair;
+
+        // Display results.
+        foreach (KeyValuePair<string, float> pair in items)
+        {
+            tempstr += "\n"+ pair.Key + ". " + pair.Value;
+            //if (pair.Key == playername){
+            //    Pos = posCount;
+            //    pos = Pos;
+            //}
+            //posCount++;
+        }
+       // PosNum.text = postionText(Pos);
+       /// RaceOrderText.text=tempstr;
+    }
+}
--- MainMenuController.cs
+++ MainMenuController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject mainMenu, modeMenu, difMenu;
+    public GameObject mainMenu, modeMenu, difMenu, timesMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +20,17 @@ public class MainMenuController : MonoBehaviour
     public void playGame(){
         mainMenu.gameObject.SetActive(false);
         modeMenu.gameObject.SetActive(true);
+        timesMenu.gameObject.SetActive(false);
     }
     public void backToMain(){
         modeMenu.gameObject.SetActive(false);
         mainMenu.gameObject.SetActive(true);
+        timesMenu.gameObject.SetActive(false);
     }
+    public void topTimes(){
+        modeMenu.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(false);
+        timesMenu.gameObject.SetActive(true);
+    }
+
 }
