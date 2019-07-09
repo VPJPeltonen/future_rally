0268adbd1f6b80b3d842596bb8d937e3d3e6e4a0
VPJP <vpjpeltonen@gmail.com>
2019-07-09 15:26:21 +0300

    parse function to high scores

--- GameData/HighScores.cs
+++ GameData/HighScores.cs
@@ -13,8 +13,16 @@ public class HighScores : MonoBehaviour
     public static List<float> TTtimes = new List<float>();
     public static List<float> LTtimes = new List<float>();
 
-    public static Dictionary<string,float> TTscores=  new Dictionary<string, float>();
-    public static Dictionary<string,float> LTscores=  new Dictionary<string, float>();
+    public static Dictionary<string,float> TTscores=  new Dictionary<string, float>(){
+        {"ab",100f},
+        {"bs",200f},
+        {"bww",20f}
+    };
+    public static Dictionary<string,float> LTscores=  new Dictionary<string, float>(){
+        {"ab",100f},
+        {"bs",200f},
+        {"bww",20f}
+    };
 
     private void orderscores(Dictionary<string,float> list){
         string tempstr = "";
@@ -40,24 +48,7 @@ public class HighScores : MonoBehaviour
  
     public static string getTopNames(string selection){
         string tempstr = "";
-        IOrderedEnumerable<KeyValuePair<string, float>> items;
-        switch(selection){
-            case "lap":
-                items = from pair in LTscores
-                            orderby pair.Value ascending
-                            select pair;
-                break;
-            case "total":
-                items = from pair in TTscores
-                    orderby pair.Value ascending
-                    select pair;
-                break;
-            default:
-                items = from pair in TTscores
-                    orderby pair.Value ascending
-                    select pair;
-                break;
-        }
+        IOrderedEnumerable<KeyValuePair<string, float>> items = orderLists(selection);
         foreach (KeyValuePair<string, float> pair in items)
         {
             tempstr += "\n"+ pair.Key;
@@ -66,6 +57,14 @@ public class HighScores : MonoBehaviour
     }
     public static string getTopTimes(string selection){
         string tempstr = "";
+        IOrderedEnumerable<KeyValuePair<string, float>> items = orderLists(selection);
+        foreach (KeyValuePair<string, float> pair in items)
+        {
+            tempstr += "\n"+ parseTime(pair.Value);
+        }
+        return tempstr;
+    }
+    private static IOrderedEnumerable<KeyValuePair<string, float>> orderLists(string selection){
         IOrderedEnumerable<KeyValuePair<string, float>> items;
         switch(selection){
             case "lap":
@@ -84,13 +83,16 @@ public class HighScores : MonoBehaviour
                     select pair;
                 break;                
         }
-        
-
-
-        foreach (KeyValuePair<string, float> pair in items)
-        {
-            tempstr += "\n"+ pair.Value;
+        return items;
+    }
+    private static string parseTime(float time){
+        string minutes = ((int) time/60).ToString();
+        float secondsFloat = (float)time % 60;
+        string seconds = secondsFloat.ToString("f2");
+        if (secondsFloat <= 10){
+            seconds = "0" + secondsFloat.ToString("f2");
         }
-        return tempstr;
+        string timerTime = minutes + ":" + seconds;  
+        return timerTime;
     }
 }
